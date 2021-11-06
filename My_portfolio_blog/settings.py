77d67ae39f632b5110eb1f432dc8bb28b21ffc09
yukty2000 <yukty2000@gmail.com>
2021-11-06 10:07:44 +0530

    Commit 6

--- My_portfolio_blog/settings.py
+++ My_portfolio_blog/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +124,5 @@ USE_TZ = True
 
 STATIC_ROOT =  os.path.join(BASE_DIR , 'staticfiles')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
