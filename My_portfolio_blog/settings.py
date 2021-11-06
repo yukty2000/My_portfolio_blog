75ce4767f8c04edc6a97469897a256040055f2dc
yukty2000 <yukty2000@gmail.com>
2021-11-06 09:23:38 +0530

    Commit 4

--- My_portfolio_blog/settings.py
+++ My_portfolio_blog/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -119,4 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT =  os.path.join(BASE_DIR , 'staticfiles')
 STATIC_URL = '/static/'
